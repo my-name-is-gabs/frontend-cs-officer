5954070a3579630f47543121dfb225049219952d
my-name-is-gabs <villar.gabriel.p.6@gmail.com>
2023-11-27 17:33:36 +0800

    url added

--- src/constant/index.js
+++ src/constant/index.js
@@ -3,5 +3,6 @@ export const MAX_FORM_COUNT = 4;
 export const INITIAL_FORM_COUNT = 1;
 
 // AXIOS API CONSTANTS
-export const BASE_URL = "http://127.0.0.1:8000";
+// export const BASE_URL = "http://127.0.0.1:8000";
+export const BASE_URL = "https://centrosecretariat-docker.onrender.com";
 export const API_TIMEOUT = 5000;
